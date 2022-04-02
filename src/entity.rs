83cc0585bb8c11ec4289b5f729bf4e5fa5b8e435
Markus Zehnder <markus.z@unfoldedcircle.com>
2022-04-03 01:03:51 +0200

    chore: clippy

--- src/entity.rs
+++ src/entity.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use serde_with::skip_serializing_none;
 
-use crate::{RE_ICON_ID, RE_ID_CHARS};
+use crate::RE_ID_CHARS;
 
 /// Execute an entity command.
 ///
